ea410de9d9c0a879e09a4dd635de50e56ccbc9a5
ha272won <103228431+ha272won@users.noreply.github.com>
2022-05-10 23:33:33 -0700

    update

--- skillDemo.java
+++ skillDemo.java
@@ -1,3 +1,5 @@
 public class skillDemo {
-  
+    public static int add(int n, int m) {
+        return n + m;
+    }
 }
---
+++ skillDemoTester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skillDemoTester {
+    @Test
+    public void addition() {
+        assertEquals(2, skillDemo.add(1, 2));
+    }
+}
